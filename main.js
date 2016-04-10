ba80d955ed8c266d637132e23877e3c927e8e6de
Steph Huerta <stph.flwrs@gmail.com>
2016-04-09 21:10:36 -0400

    I think slugs work now

--- main.js
+++ main.js
@@ -46,9 +46,9 @@ var hackathonPage = function (hackathon, page, filters) {
                 var title = $( $(element).find('figcaption > div > h5') ).text().trim();
                 var desc = $( $(element).find('figcaption > div > p') ).text().trim();
                 var teamSize = $(element).find('.user-profile-link').length;
-                var slug = $('.link-to-software').attr('href');
+                var slug = $( $(element).find('.link-to-software') ).attr('href');
 
-                var re = /^.*software\/()\/{0,1}$/;
+                var re = /^.*software\/(.*)\/{0,1}$/;
                 slug = slug.match(re)[1];
                 data.push({
                     'url': link,
@@ -175,6 +175,8 @@ var projectFindBySlug = function (projectSlug) {
 
     return deferred.promise;
 };
+
+hackathonPage('bitcamp15', 1);
 
 module.exports = {
     hackathon: {
